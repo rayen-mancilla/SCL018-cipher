70a61b1ff80a45009e30cbe968c926a8782226c6
rayen-mancilla <89403004+rayen-mancilla@users.noreply.github.com>
2021-09-06 15:40:39 -0300

    Css 2 listo y display

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+//Declarando variables para luego aplicar display block& none
+
+let unveilWelcomePage= document.getElementById("welcomePage")
+unveilWelcomePage.style.display="block"
+let unveilMainPage= document.getElementById ("mainPage");
+unveilMainPage.style.display="none";
+
+// Creando función para pasar a Main Page, haciendo click en el botón "start"
+
+const startButton = document.getElementById("startButton");
+startButton.addEventListener('click', startEncrypta);
+function startEncrypta() {
+  unveilWelcomePage.style.display ="none";
+  unveilMainPage.style.display ="block";
+}
+
+
+
+//const startButton= document.getElementById("startButton");
+//startButton.addEventListener("click" , startEncrypta);
+//function startEncrypta(){
+ //   unveilWelcomePage.style.display= "none" ;
+  //  unveilMainPage.style.display= "block" ;
+//}
